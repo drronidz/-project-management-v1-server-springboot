52b612ed3d0dab2dcb31c54e9a59cd0b39bb704f
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-27 00:01:13 +0100

    Project Validation PART 1 ...

--- src/main/java/com/cleverdeveloper/ppmtool/controllers/ProjectController.java
+++ src/main/java/com/cleverdeveloper/ppmtool/controllers/ProjectController.java
@@ -5,7 +5,11 @@ import com.cleverdeveloper.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 /*
 PROJECT NAME : ppmtool
@@ -29,7 +33,12 @@ public class ProjectController {
 
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity <?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return new ResponseEntity<String>("Invalid Project Object ", HttpStatus.BAD_REQUEST);
+        }
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
