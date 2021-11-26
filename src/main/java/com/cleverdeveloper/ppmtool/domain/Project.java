e3829c0d8932ed2be748d693405374a14c13331d
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-26 22:37:26 +0100

    Setting UP project Files: 1. H2-Databases (Creating file with mv.db extension) 2. Creating a Project Object & Project Repository ...

---
+++ src/main/java/com/cleverdeveloper/ppmtool/domain/Project.java
@@ -0,0 +1,105 @@
+package com.cleverdeveloper.ppmtool.domain;
+
+/*
+PROJECT NAME : ppmtool
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 11/26/2021 10:10 PM
+*/
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+
+    private Date createdAt;
+    private Date updatedAt;
+
+    public Project() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
+
+}
---
+++ src/main/java/com/cleverdeveloper/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,19 @@
+package com.cleverdeveloper.ppmtool.repositories;
+
+import com.cleverdeveloper.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/*
+PROJECT NAME : ppmtool
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 11/26/2021 10:34 PM
+*/
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> longs);
+}
