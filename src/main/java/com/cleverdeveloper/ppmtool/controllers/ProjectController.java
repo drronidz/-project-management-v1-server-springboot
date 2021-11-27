c886d646ef969f64733699955559f7fe1ac68131
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-27 14:27:03 +0100

    Delete an Existing Project  ...

--- src/main/java/com/cleverdeveloper/ppmtool/controllers/ProjectController.java
+++ src/main/java/com/cleverdeveloper/ppmtool/controllers/ProjectController.java
@@ -58,4 +58,11 @@ public class ProjectController {
         return projectService.findAllProjects();
     }
 
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId) {
+        projectService.deleteProjectByIdentifier(projectId);
+
+        return new ResponseEntity<>("Project with ID: " + projectId + " was deleted", HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/cleverdeveloper/ppmtool/services/ProjectService.java
+++ src/main/java/com/cleverdeveloper/ppmtool/services/ProjectService.java
@@ -46,4 +46,14 @@ public class ProjectService {
     public Iterable<Project> findAllProjects() {
         return projectRepository.findAll();
     }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Project ID (To Delete ...):" + projectId + " does not exist");
+        }
+
+        projectRepository.delete(project);
+    }
 }
